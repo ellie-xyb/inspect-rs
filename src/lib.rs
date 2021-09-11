4622d2d0ec906962deaf447dd19e69865d1b4eb9
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-09-11 23:28:40 +0900

    warm up with basic enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
+enum Color {
+    Blue,
+    Green,
+    Red,
+}
+
+pub fn test() {
+    let a = Color::Blue;
+    match a{
+        Color::Blue => println!("blue"),
+        Color::Green => todo!(),
+        Color::Red => todo!(),
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::test;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        test();
     }
 }
