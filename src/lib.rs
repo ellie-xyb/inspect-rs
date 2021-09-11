22ce3fc91cb31e3233ebbeae1ff5d20c750d7024
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-09-11 23:52:43 +0900

    play with traits and generic

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
-enum Color {
-    Blue,
-    Green,
-    Red,
+struct Job {
+    name: String,
+}
+
+enum Status<T> {
+    Pending(T),
+    Accepted(T),
+    Rejected(T),
+}
+
+fn print_status<T: ToString>(status: Status<T>) {
+    match status {
+        Status::Pending(t) => println!("p: {}", t.to_string()),
+        Status::Accepted(t) => println!("a: {}", t.to_string()),
+        Status::Rejected(t) => println!("r: {}", t.to_string()),
+    }
 }
 
 pub fn test() {
-    let a = Color::Blue;
-    match a{
-        Color::Blue => println!("blue"),
-        Color::Green => todo!(),
-        Color::Red => todo!(),
-    }
+    let a = Status::Pending("damn bear!");
+    print_status(a);
 }
 
 #[cfg(test)]
