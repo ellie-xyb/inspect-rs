025b98dc48abfeb2d05c4801163a8324e012ac89
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-09-12 00:55:14 +0900

    implement InspectResult trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,61 @@
-struct Job {
-    name: String,
-    age: usize,
+
+pub trait InspectableResult {
+    fn inspect_ok(self) -> Self;
+    fn inspect_err(self) -> Self;
+    fn inspect_both(self) -> Self;
 }
 
-impl ToString for Job {
-    fn to_string(&self) -> String {
-        format!("{} / {}", self.name, self.age)
+impl<T, E> InspectableResult for Result<T, E> {
+    fn inspect_ok(self) -> Self {
+        if self.is_ok() {
+            println!("It's OK!");
+        }
+        self
     }
-}
 
-enum Status<T> {
-    Pending(T),
-    Accepted(T),
-    Rejected(T),
-}
-
-fn print_status<T: ToString>(status: Status<T>) {
-    match status {
-        Status::Pending(t) => println!("p: {}", t.to_string()),
-        Status::Accepted(t) => println!("a: {}", t.to_string()),
-        Status::Rejected(t) => println!("r: {}", t.to_string()),
+    fn inspect_err(self) -> Self {
+        if self.is_err() {
+            print!("It's an error");
+        }
+        self
     }
-}
 
-pub fn test() {
-    let a = Status::Pending(Job {
-        name: "damn bear!".to_string(),
-        age: 100,
-    });
-    print_status(a);
+    fn inspect_both(self) -> Self {
+        match self {
+            Ok(_) => self.inspect_ok(),
+            Err(_) => self.inspect_err(),
+        }
+    }
+
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::test;
+    use crate::InspectableResult;
+
+    fn test_success() -> Result<usize, String> {
+        Ok(0)
+    }
+    
+    fn test_error() -> Result<usize, String> {
+        Err("I'm broken".to_string())
+    }
+
     #[test]
-    fn it_works() {
-        test();
+    fn err_returns_both() {
+        assert!(test_success().inspect_err().is_ok());
+        assert!(test_error().inspect_err().is_err());
+    }
+
+    #[test]
+    fn ok_returns_both() {
+        assert!(test_success().inspect_ok().is_ok());
+        assert!(test_error().inspect_ok().is_err());
+    }
+
+    #[test]
+    fn both_returns_both() {
+        assert!(test_success().inspect_both().is_ok());
+        assert!(test_error().inspect_both().is_err());
     }
 }
